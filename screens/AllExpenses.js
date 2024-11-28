abb121d2dcc74722dcba07db639b0b525bfaab50
Dylan Sung <dys907@gmail.com>
2024-11-27 17:38:23 -0800

    dummy expenses

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,10 +1,7 @@
-import { Text } from "react-native"
-
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput"
 
 const AllExpenses = (params) => {
-    return (
-        <Text>All Expense Screen</Text>
-    )
+    return <ExpensesOutput expensesPeriod={"Last 7 Days"}/>
 }
 
 export default AllExpenses
